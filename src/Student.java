c8d2e2750abb01cb10d5622b91d2ddf625fdffa0
Muhammed <mmusabgldmr@gmail.com>
2025-12-31 13:09:23 +0300

    .
    .

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,18 @@
-public class Student {
+public class Student extends User{
+    int gpa; // Oğrencinin not ortalamsı.
+
+    public Student(String name, String email, int ID, int gpa){
+        super(name, email, ID);
+        this.gpa = gpa;
+    }
+
+    // Oğrencilerin bilgilerini ekrana yazdıran metod.
+    @Override
+    public void displayInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("ID: " + ID);
+        System.out.println("GPA: " + gpa);
+    }
     
 }
