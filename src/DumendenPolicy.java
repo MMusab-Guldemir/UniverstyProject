ecfc684b10194a8b43eb8d3d5e5407e4fd59ca51
Muhammed <mmusabgldmr@gmail.com>
2026-01-01 12:21:08 +0300

    UniverstyRegistration
    .

--- src/DumendenPolicy.java
+++ src/DumendenPolicy.java
@@ -1,3 +1,9 @@
-public class DumendenPolicy {
+public class DumendenPolicy implements IRegistrationPolicy{
+
+    @Override
+    public boolean canRegister(User user, Course course){
+        System.out.println("Dumenden bir ders alma politikasi ben uydurdum");
+        return true;
+    }
     
 }
--- src/RegistrationService.java
+++ src/RegistrationService.java
@@ -1,15 +1,12 @@
-import java.lang.classfile.Signature.ThrowableSig;
-import java.security.PublicKey;
-
 public class RegistrationService {
     private IRegistrationPolicy registrationPolicy;
 
-    // constructor ile politika belirlenir.
-    public RegistrationService(IRegistrationPolicy registrationPolicy){
+    //constructor ile politika belirlenir
+    public RegistrationService(IRegistrationPolicy registrationPolicy) {
         this.registrationPolicy = registrationPolicy;
     }
 
-    public Void register(Student student, Course course){
+    public void register(Student student, Course course) {
         if(registrationPolicy.canRegister(student, course)){
             course.enroll(student);
         }
